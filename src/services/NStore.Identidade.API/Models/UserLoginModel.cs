5e4ce2976d37506b9f84c80ced8362f6a454ee9f
Lucas Lopes <lucaslopesr@outlook.com>
2021-01-24 23:25:21 -0300

    registrando usuário

---
+++ src/services/NStore.Identidade.API/Models/UserLoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NStore.Identidade.API.Models
+{
+    public class UserLoginModel
+    {
+    }
+}
---
+++ src/services/NStore.Identidade.API/Models/UserRegistroModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NStore.Identidade.API.Models
+{
+    public class UserRegistroModel
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string SenhaConfirmacao { get; set; }
+    }
+}
