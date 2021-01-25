e58eb4809caececc90c13cad7315cb9a91fd59a4
Lucas Lopes <lucaslopesr@outlook.com>
2021-01-25 13:26:26 -0300

    gerando cookies de autenticação através do JWT

--- src/web/NStore.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NStore.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using NStore.WebApp.MVC.Models;
 using NStore.WebApp.MVC.Services;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace NStore.WebApp.MVC.Controllers
@@ -29,7 +35,7 @@ namespace NStore.WebApp.MVC.Controllers
             if (!ModelState.IsValid) return View(usuario);
 
             var resposta = await autenticacaoService.Registrar(usuario);
-
+            await RealizarLogin(resposta);
             return RedirectToAction("Login", "Identidade");
         }
 
@@ -54,8 +60,32 @@ namespace NStore.WebApp.MVC.Controllers
             if (!ModelState.IsValid) return View(usuario);
 
             var resposta = await autenticacaoService.Login(usuario);
-
+            await RealizarLogin(resposta);
             return RedirectToAction("Index", "Home");
+        }
+
+        private async Task RealizarLogin(UsuarioAutenticacaoResponse resposta) 
+        {
+            var token = ObterTokenFormatado(resposta.AccessToken);
+            var claims = new List<Claim>();
+
+            claims.Add(new Claim("JWT", resposta.AccessToken));
+            claims.AddRange(token.Claims);
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60),
+                IsPersistent = true
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+        }
+
+        private static JwtSecurityToken ObterTokenFormatado(string jwtToken) 
+        {
+            return new JwtSecurityTokenHandler().ReadJwtToken(jwtToken);
         }
     }
 }
