a58fd0801175d8e00321da8598a8cd4eeed5aca4
Lucas Lopes <lucaslopesr@outlook.com>
2021-01-25 03:28:25 -0300

    configurando autenticação no mvc

---
+++ src/web/NStore.WebApp.MVC/Configuration/AuthConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NStore.WebApp.MVC.Configuration
+{
+    public static class AuthConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => 
+                {
+                    options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/acesso-negado";
+                });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            return app;
+        }
+    }
+}
