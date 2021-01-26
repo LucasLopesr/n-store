bcad79cdecaffd79fc649453582741f73699f0b0
Lucas Lopes <lucaslopesr@outlook.com>
2021-01-25 22:57:41 -0300

    entity domain object

--- src/building blocks/NStore.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NStore.Core
-{
-    public class Class1
-    {
-    }
-}
--- src/services/NStore.Catalogo.API/Startup.cs
+++ src/services/NStore.Catalogo.API/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace NStore.Catalogo.API
 {
@@ -23,7 +17,6 @@ namespace NStore.Catalogo.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -34,7 +27,6 @@ namespace NStore.Catalogo.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
