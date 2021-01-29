24aa4519007206ca752aa471a9eab5f6013c54c9
Lucas Lopes <lucaslopesr@outlook.com>
2021-01-28 22:08:45 -0300

    ajustando rota padrão

--- src/web/NStore.WebApp.MVC/Configuration/WebAppConfig.cs
+++ src/web/NStore.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -42,7 +42,7 @@ namespace NStore.WebApp.MVC.Configuration
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Catalogo}/{action=Index}/{id?}");
             });
             return app;
         }
