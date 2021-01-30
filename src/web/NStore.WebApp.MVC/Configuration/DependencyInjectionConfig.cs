d144cdaaa0f3ded4ffacc0dcdd9c8a78ee13648a
Lucas Lopes <lucaslopesr@outlook.com>
2021-01-29 22:05:32 -0300

    Retry Pattern com Polly

--- src/web/NStore.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NStore.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -4,20 +4,36 @@ using Microsoft.Extensions.DependencyInjection;
 using NStore.WebApp.MVC.Extensions;
 using NStore.WebApp.MVC.Services;
 using NStore.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
 using System;
 
 namespace NStore.WebApp.MVC.Configuration
 {
     public static class DependencyInjectionConfig
     {
-        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration) 
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<HttpClientAutorizationDelegatingHandler>();
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
+
+            var retryWaitPolice = HttpPolicyExtensions.HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10)
+                }, (outcome, timespan, retryCount, context) =>
+                {
+                    Console.WriteLine($"Número de tentativas: {retryCount}");
+                });
+
             services.AddHttpClient<ICatalogoService, CatalogoService>()
-                .AddHttpMessageHandler<HttpClientAutorizationDelegatingHandler>();
+                .AddHttpMessageHandler<HttpClientAutorizationDelegatingHandler>()
+                // .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(retryCount: 3, sleepDurationProvider: _=> TimeSpan.FromMilliseconds(600)));
+                .AddPolicyHandler(retryWaitPolice);
 
             //Exemplo de configuração do refit 
             //services.AddHttpClient("Refit", options => {
@@ -26,7 +42,7 @@ namespace NStore.WebApp.MVC.Configuration
             //.AddTypedClient(Refit.RestService.For<ICatalogoService>);
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            
+
             services.AddScoped<IUser, AspNetUser>();
         }
 
