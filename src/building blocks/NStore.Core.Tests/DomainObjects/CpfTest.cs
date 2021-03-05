d376ae7a174809f802740625a7be20965123edb6
Lucas Lopes <lucaslopesr@outlook.com>
2021-03-04 22:58:33 -0300

    ajuste encoding

--- src/building blocks/NStore.Core.Tests/DomainObjects/CpfTest.cs
+++ src/building blocks/NStore.Core.Tests/DomainObjects/CpfTest.cs
@@ -6,29 +6,29 @@ using Xunit;
 
 namespace NStore.Core.Tests.DomainObjects
 {
-    /// <summary>
-    /// Padrão de nomenclatura - MetodoEmTeste_EstadoEmTeste_ComportamentoExperado
-    /// </summary>
-    public class CpfTest
-    {
-        [Fact(DisplayName = "CPF Válido")]
-        [Trait("Categoria", "Testes de CPF")]
-        public void Validar_CpfValido_DeveRetornarVerdadeiro()
-        {
-            Assert.True(Cpf.Validar("385.235.940-60"));
-        }
+	/// <summary>
+	/// Padrão de nomenclatura - MetodoEmTeste_EstadoEmTeste_ComportamentoExperado
+	/// </summary>
+	public class CpfTest
+	{
+		[Fact(DisplayName = "CPF Válido")]
+		[Trait("Categoria", "Testes de CPF")]
+		public void Validar_CpfValido_DeveRetornarVerdadeiro()
+		{
+			Assert.True(Cpf.Validar("385.235.940-60"));
+		}
 
-        [Fact(DisplayName = "CPF Inválido")]
-        [Trait("Categoria", "Testes de CPF")]
-        public void Validar_CpfInvalido_DeveGerarException()
-        {
+		[Fact(DisplayName = "CPF Inválido")]
+		[Trait("Categoria", "Testes de CPF")]
+		public void Validar_CpfInvalido_DeveGerarException()
+		{
 
-            //Act
+			//Act
 
-            var ex = Assert.Throws<DomainException>(() => new Cpf("12332154544"));
+			var ex = Assert.Throws<DomainException>(() => new Cpf("12332154544"));
 
-            //Assert
-            Assert.Equal("Cpf inválido", ex.Message);
-        }
-    }
+			//Assert
+			Assert.Equal("Cpf inválido", ex.Message);
+		}
+	}
 }
